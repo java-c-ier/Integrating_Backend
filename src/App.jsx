09ff07bb6435e003171276680eaf8f7d9a5729ea
java-c-ier <jimutsahoo02@gmail.com>
2024-08-12 18:00:05 +0530

    Links and Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,26 @@
 import React, { useState } from "react";
-import axios from "axios";
+import Home from "./Components/Home";
+import Show from "./Components/Show";
+import Services from "./Components/Services";
+import { Link, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function App() {
-
-  const [products, setProducts] = useState([])
-
-
-  const getProducts = () => {
-    const api = "https://fakestoreapi.com/products";
-    axios
-      .get(api)
-      .then((products) => {
-        // console.log(products);
-        setProducts(products.data)
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const addProducts = () => {
-    const api = "https://fakestoreapi.com/products";
-    axios
-      .post(api, {
-        title: 'test product',
-        price: 13.5,
-        description: 'lorem ipsum set',
-        image: 'https://i.pravatar.cc',
-        category: 'electronic'
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  console.log(products);
-  
   return (
-    <div className="p-[5%]">
-      <button onClick={getProducts} className="px-3 py-1 rounded-full bg-blue-500 text-white text-semibold">Get Product</button>
+    <div className="p-[3%]">
+      <nav className="flex justify-center gap-20">
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/show">Show</Link>
+      </nav>
 
-      <br /><br />
+      {/* <hr className="h-[1.5px] w-full mt-5 color-black"/> */}
 
-      <button onClick={addProducts} className="px-3 py-1 rounded-full bg-blue-500 text-white text-semibold">Add Product</button>
-
-      <hr className="h-0.5 w-11/12 my-10 bg-black" />
-
-      <ul className="flex gap-5 flex-wrap">
-        {products.length > 0 ? products.map( p => <li key={p.id} className="w-40 rounded-lg bg-red-300 p-3 truncate">{p.title}</li>) : <h1>Loading...</h1>}
-      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/show" element={<Show />}></Route>
+      </Routes>
     </div>
   );
 }
